371df8edfff8fd7e5a14639a38f30ffa4789a1be
jack-mccabe-defra <jack.mccabe@defra.gov.uk>
2025-05-19 17:17:49 +0100

    set introspection + update tests to use mocks

--- test/unit/integration/narrow/routes/graphql-dashboard.test.js
+++ test/unit/integration/narrow/routes/graphql-dashboard.test.js
@@ -1,25 +1,75 @@
-import { apolloServer, enableApolloLandingPage } from '../../../../../app/graphql/server.js'
+import { jest } from '@jest/globals'
 
-describe('GraphQL Dashboard test', () => {
-  it('expects graphql dashboard to be enabled when GRAPHQL_DASHBOARD_ENABLED is set in .env.test', async () => {
-    expect(apolloServer.internals.plugins.length).toBe(1)
-    expect(apolloServer.internals.plugins[0].__is_disabled_plugin__).toBeUndefined()
+describe('GraphQL Dashboard test with mocks', () => {
+  const mockApolloServer = jest.fn()
+  const mockApolloModule = {
+    ApolloServer: mockApolloServer
+  }
+  jest.unstable_mockModule('@apollo/server', () => mockApolloModule)
+
+  const mockSchemaModule = {
+    createSchema: jest.fn()
+  }
+  jest.unstable_mockModule('../../../../../app/graphql/schema.js', () => mockSchemaModule)
+
+  const mockApolloPluginDisabled = jest.fn()
+  const mockApolloPluginDisabledModule = {
+    ApolloServerPluginLandingPageDisabled: mockApolloPluginDisabled
+  }
+  jest.unstable_mockModule('@apollo/server/plugin/disabled', () => mockApolloPluginDisabledModule)
+
+  const mockApolloPluginLandingPage = jest.fn()
+  const mockApolloPluginLandingPageModule = {
+    ApolloServerPluginLandingPageLocalDefault: mockApolloPluginLandingPage
+  }
+  jest.unstable_mockModule(
+    '@apollo/server/plugin/landingPage/default',
+    () => mockApolloPluginLandingPageModule
+  )
+
+  beforeEach(() => {
+    jest.resetModules()
   })
 
-  it('enables graphql dashboard when GRAPHQL_DASHBOARD_ENABLED is enabled', async () => {
-    process.env.GRAPHQL_DASHBOARD_ENABLED = 'true'
-
-    const response = enableApolloLandingPage()
-
-    expect(response.__is_disabled_plugin__).toBeUndefined()
-  })
-
-  it('disables graphql dashboard when GRAPHQL_DASHBOARD_ENABLED is disabled', async () => {
+  it('expect ApolloServer to have been called with correct args to disable landing page', async () => {
     process.env.GRAPHQL_DASHBOARD_ENABLED = 'false'
+    mockSchemaModule.createSchema.mockReturnValue('mockCreateSchemaRV')
+    const { apolloServer } = await import('../../../../../app/graphql/server.js')
 
-    const response = enableApolloLandingPage()
+    expect(apolloServer).toBeDefined()
+    expect(mockSchemaModule.createSchema).toHaveBeenCalledTimes(1)
+    expect(mockSchemaModule.createSchema).toHaveBeenCalledWith()
 
-    expect(response.__is_disabled_plugin__).toBeDefined()
-    expect(response.__is_disabled_plugin__).toBe(true)
+    expect(mockApolloPluginDisabled).toHaveBeenCalledTimes(1)
+    expect(mockApolloPluginDisabled).toHaveBeenCalledWith()
+
+    expect(mockApolloPluginLandingPage).not.toHaveBeenCalled()
+    expect(mockApolloServer).toHaveBeenCalledWith({
+      schema: 'mockCreateSchemaRV',
+      plugins: [mockApolloPluginDisabled()],
+      introspection: false
+    })
+  })
+
+  it('expect ApolloServer to have been called with correct args to enable landing page', async () => {
+    process.env.GRAPHQL_DASHBOARD_ENABLED = 'true'
+    mockSchemaModule.createSchema.mockReturnValue('mockCreateSchemaRV')
+    const { apolloServer } = await import('../../../../../app/graphql/server.js')
+
+    expect(apolloServer).toBeDefined()
+
+    expect(mockSchemaModule.createSchema).toHaveBeenCalledTimes(1)
+    expect(mockSchemaModule.createSchema).toHaveBeenCalledWith()
+
+    expect(mockApolloPluginDisabled).not.toHaveBeenCalled()
+
+    expect(mockApolloPluginLandingPage).toHaveBeenCalledTimes(1)
+    expect(mockApolloPluginLandingPage).toHaveBeenCalledWith()
+
+    expect(mockApolloServer).toHaveBeenCalledWith({
+      schema: 'mockCreateSchemaRV',
+      plugins: [mockApolloPluginLandingPage()],
+      introspection: true
+    })
   })
 })
