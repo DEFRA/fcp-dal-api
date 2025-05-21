c9ac90559da00dd7456ab1b8503a1ee5d58211af
Robin Knipe <robin.knipe@defra.gov.uk>
2025-05-21 18:05:12 +0100

    fix: correct implementation and test

--- app/logger/sendMetric.js
+++ app/logger/sendMetric.js
@@ -2,27 +2,26 @@ import { createMetricsLogger, StorageResolution, Unit } from 'aws-embedded-metri
 import { DAL_METRICS_ERROR_001 } from './codes.js'
 import { logger } from './logger.js'
 
-if(process.env.NODE_ENV==='production') {
-export async function sendMetric(
-  metricName,
-  value = 1,
-  unit = Unit.Count,
-  dimensions = {},
-  storageResolution = StorageResolution.Standard
-) {
-  try {
-    const metricsLogger = createMetricsLogger()
-    if (Object.keys(dimensions).length > 0) {
-      metricsLogger.setDimensions(dimensions)
-    }
-    metricsLogger.putMetric(metricName, value, unit, storageResolution)
-    await metricsLogger.flush()
-  } catch (error) {
-    logger.error('#DAL - failed to send metric', { error, code: DAL_METRICS_ERROR_001 })
-  }
-}
-} else {
-  export async function sendMetric() {
-    return Promise.resolve()
-  }
-}
+const sendMetric =
+  process.env.NODE_ENV === 'production'
+    ? async (
+        metricName,
+        value = 1,
+        unit = Unit.Count,
+        dimensions = {},
+        storageResolution = StorageResolution.Standard
+      ) => {
+        try {
+          const metricsLogger = createMetricsLogger()
+          if (Object.keys(dimensions).length > 0) {
+            metricsLogger.setDimensions(dimensions)
+          }
+          metricsLogger.putMetric(metricName, value, unit, storageResolution)
+          await metricsLogger.flush()
+        } catch (error) {
+          logger.error('#DAL - failed to send metric', { error, code: DAL_METRICS_ERROR_001 })
+        }
+      }
+    : async () => Promise.resolve()
+
+export { sendMetric }
