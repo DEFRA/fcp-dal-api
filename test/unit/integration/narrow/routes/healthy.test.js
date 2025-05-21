b7b96eae6ca9aba08142ab2f2758af1e99d9ca3e
jack-mccabe-defra <jack.mccabe@defra.gov.uk>
2025-05-21 18:51:56 +0100

    fix healthy tests

--- test/unit/integration/narrow/routes/healthy.test.js
+++ test/unit/integration/narrow/routes/healthy.test.js
@@ -1,29 +1,38 @@
 import { jest } from '@jest/globals'
 import { RuralPaymentsBusiness } from '../../../../../app/data-sources/rural-payments/RuralPaymentsBusiness.js'
-import { server } from '../../../../../app/server.js'
-// import throttle from '../../../../../app/utils/throttle.js'
+
+const mockThrottle = (fn) => {
+  return async (...args) => {
+    return await fn(...args)
+  }
+}
+
+const mockThrottleModule = {
+  throttle: mockThrottle
+}
+
+jest.unstable_mockModule('../../../../../app/utils/throttle.js', () => mockThrottleModule)
+
+const { server } = await import('../../../../../app/server.js')
 
 describe('Healthy test', () => {
+  const mockGetOrganisationById = jest.spyOn(RuralPaymentsBusiness.prototype, 'getOrganisationById')
+
   beforeEach(async () => {
     process.env.HEALTH_CHECK_ENABLED = 'true'
     process.env.HEALTH_CHECK_RP_INTERNAL_ORGANISATION_ID = 'test-org-id'
     process.env.RURAL_PAYMENTS_PORTAL_EMAIL = 'test@example.com'
     process.env.HEALTH_CHECK_RURAL_PAYMENTS_THROTTLE_TIME_MS = 1
-    // jest.spyOn(throttle, 'throttle').mockImplementation(() => {
-    //   return (fn) => {
-    //     return fn()
-    //   }
-    // })
+
     await server.start()
   })
 
   afterEach(async () => {
     await server.stop()
-    jest.resetAllMocks()
   })
 
   it('GET /healthy route returns 200 with services status when health check is enabled', async () => {
-    jest.spyOn(RuralPaymentsBusiness.prototype, 'getOrganisationById').mockResolvedValue({
+    mockGetOrganisationById.mockResolvedValue({
       id: 'test-org-id'
     })
 
@@ -73,7 +82,7 @@ describe('Healthy test', () => {
   })
 
   it('GET /healthy route returns 200 with services status when RuralPaymentsPortal is down', async () => {
-    jest.spyOn(RuralPaymentsBusiness.prototype, 'getOrganisationById').mockResolvedValue(null)
+    mockGetOrganisationById.mockResolvedValue(null)
 
     const options = {
       method: 'GET',
@@ -89,9 +98,7 @@ describe('Healthy test', () => {
   })
 
   it('GET /healthy route returns 500 when RuralPaymentsPortal check throws error', async () => {
-    jest
-      .spyOn(RuralPaymentsBusiness.prototype, 'getOrganisationById')
-      .mockRejectedValue(new Error('API Error'))
+    mockGetOrganisationById.mockRejectedValue(new Error('API Error'))
 
     const options = {
       method: 'GET',
