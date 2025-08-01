ff94c6e522cfa9a3e770e9a8c888d59b49ceed8e
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2025-08-01 10:56:09 +0100

    add mutations to update customers

---
+++ app/graphql/resolvers/customer/mutation.js
@@ -0,0 +1,24 @@
+import { transformCustomerUpdateInputToPersonUpdate } from '../../../transformers/rural-payments/customer.js'
+
+async function updateCustomerResolver(_, { input }, { dataSources }) {
+  const personId = await dataSources.ruralPaymentsCustomer.getPersonIdByCRN(input.crn)
+  const person = await dataSources.ruralPaymentsCustomer.getPersonByPersonId(personId)
+
+  await dataSources.ruralPaymentsCustomer.updatePersonDetails(
+    personId,
+    transformCustomerUpdateInputToPersonUpdate(person, input)
+  )
+
+  return {
+    success: true,
+    customer: { crn: input.crn }
+  }
+}
+
+export const Mutation = {
+  updateCustomerAddress: updateCustomerResolver,
+  updateCustomerDateOfBirth: updateCustomerResolver,
+  updateCustomerEmail: updateCustomerResolver,
+  updateCustomerName: updateCustomerResolver,
+  updateCustomerPhone: updateCustomerResolver
+}
