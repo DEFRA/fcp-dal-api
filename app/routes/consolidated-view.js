312192ae7053527eb151dfb56207ed3e5d7b0f41
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2025-09-29 21:20:08 +0100

    add linked contacts page

--- app/routes/consolidated-view.js
+++ app/routes/consolidated-view.js
@@ -1,3 +1,7 @@
+import { graphql } from 'graphql'
+import { context } from '../graphql/context.js'
+import { schema } from '../graphql/server.js'
+
 export const consolidatedViewRoutes = (cssPath) => [
   {
     method: 'GET',
@@ -23,13 +27,84 @@ export const consolidatedViewRoutes = (cssPath) => [
   },
   {
     method: 'GET',
-    path: '/consolidated-view/linked-contacts',
-    handler: (_request, h) => {
-      const context = {
+    path: '/consolidated-view/linked-contacts/{sbi}',
+    handler: async (request, h) => {
+      // Get list of customer businesses
+      const listResult = await graphql({
+        source: `#graphql
+          query LinkedContactsPage($sbi: ID!) {
+            business(sbi: $sbi) {
+              sbi
+              customers {
+                personId
+                firstName
+                lastName
+                crn
+                role
+              }
+            }
+          }
+        `,
+        schema,
+        contextValue: await context({
+          request: {
+            headers: {
+              email: 'test@defra.gov.uk',
+              'gateway-type': 'internal'
+            }
+          }
+        }),
+        variableValues: { sbi: request.params.sbi }
+      })
+
+      // Get first customer from the list
+      const selectedResult = await graphql({
+        source: `#graphql
+          query LinkedCustomerPermissions($sbi: ID!, $crn: ID!) {
+            business(sbi: $sbi) {
+              customer(crn: $crn) {
+                role
+                permissionGroups {
+                  id
+                  level
+                  functions
+                }
+              }
+            }
+            customer(crn: $crn) {
+              crn
+              info {
+                name {
+                  title
+                  first
+                  middle
+                  last
+                }
+                dateOfBirth
+              }
+            }
+          }
+        `,
+        schema,
+        contextValue: await context({
+          request: {
+            headers: {
+              email: 'test@defra.gov.uk',
+              'gateway-type': 'internal'
+            }
+          }
+        }),
+        variableValues: {
+          sbi: request.params.sbi,
+          crn: listResult.data.business.customers[0].crn
+        }
+      })
+
+      return h.view('linked-contacts', {
         title: 'Linked Contacts',
-        items: ['Item 1', 'Item 2', 'Item 3']
-      }
-      return h.view('linked-contacts', context)
+        listResult,
+        selectedResult
+      })
     }
   },
   {
