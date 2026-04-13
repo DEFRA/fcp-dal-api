1b32817376a1c18c49df9bd1c3755671411731c6
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2026-04-13 14:36:56 +0100

    fix: handle 0 and null returns in convertSquareMetresToHectares (#196)

--- app/utils/numbers.js
+++ app/utils/numbers.js
@@ -1,3 +1,6 @@
 const squareMetersToHectares = 0.0001
-export const convertSquareMetersToHectares = (area) =>
-  parseFloat((parseFloat(area) * squareMetersToHectares).toFixed(4)) || 0
+
+export const convertSquareMetersToHectares = (area) => {
+  const result = Number.parseFloat((Number.parseFloat(area) * squareMetersToHectares).toFixed(4))
+  return Number.isNaN(result) ? null : result
+}
