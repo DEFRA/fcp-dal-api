fc69e57c6ee7413afeb60d27341e81dd2530e899
Paul Gordon <scgpltd@gmail.com>
2026-07-17 14:40:21 +0100

    chore: remove placeholder refdata impl

--- app/data-sources/rural-payments/RuralPaymentsReference.js
+++
@@ -1,9 +0,0 @@
-import { RuralPayments } from './RuralPayments.js'
-
-// TODO This is a placeholder until ref data is delivered
-export class RuralPaymentsReference extends RuralPayments {
-  async legalStatus() {
-    const response = await this.get(`reference/legalstatus`)
-    return response._data
-  }
-}
