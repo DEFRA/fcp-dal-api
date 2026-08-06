235e2e9dfce0867b5888c734a492df49b9a10d98
David Cohen <david.cohen@defra.gov.uk>
2026-08-06 17:37:02 +0100

    chore: removed debug timing log
    * chore: removed debug timing log
    * chore: bumped version to 2.18.3
    * chore: removed CUSTOM_TAG from publish-hotfix.yml

--- app/data-sources/BaseRESTDataSource.js
+++ app/data-sources/BaseRESTDataSource.js
@@ -120,7 +120,6 @@ export class BaseRESTDataSource extends RESTDataSource {
       response
     })
 
-    const debugLogStart = performance.now()
     this.logger.debug(`#datasource - ${this.name} - response detail`, {
       request: { ...request, url: url.toString() },
       response: {
@@ -129,12 +128,6 @@ export class BaseRESTDataSource extends RESTDataSource {
       },
       code: this.code,
       requestTimeMs
-    })
-
-    const duration = performance.now() - debugLogStart
-    this.logger.info(`#datasource - ${this.name} - debug log duration`, {
-      code: this.code,
-      requestTimeMs: duration
     })
 
     return result
